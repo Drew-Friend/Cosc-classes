087d3ef90e67fc339d9f4a220ccac46fafd22962
Drew-Friend <dfriend414@gmail.com>
2022-05-10 09:54:11 -0400

    202 lab 7, HW 12. Did not complete HW 13, lab 8 incoming

---
+++ 202/labs/7/Goldbach.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class prime_partition {
+  public:
+    // constructor
+    prime_partition();
+    // operator(int)
+    void operator()(int);
+
+  private:
+    void create_pset();
+    bool find_partition(int);
+
+    set<int> pset;
+    // miscl member data
+    int maxPrime;
+    int minPrime;
+    vector<int> vals;
+    set<int>::iterator last;
+};
+
+int main(int argc, char *argv[])
+{
+  prime_partition goldbach;
+  int number;
+  while (1) 
+  {
+    cout << "number = ";
+      cin >> number;
+    if (cin.eof())
+      break;
+    
+    goldbach(number);
+  }
+
+  cout << "\n";
+}
+
+prime_partition::prime_partition() {
+  maxPrime, minPrime = 2,2;
+  pset.insert(minPrime);
+  create_pset();
+  last = pset.find(maxPrime);
+}
+
+void prime_partition::create_pset() {
+  int counter = maxPrime;
+  while (maxPrime <= 20000)
+  {
+    counter++;
+    int root = ceil(sqrt(counter));
+    for (int i = 2; i <= root; ++i)
+    {
+      if (!(counter % i) || maxPrime == counter)
+        break;
+      if (i == root)
+      {
+        pset.insert(counter);
+        maxPrime = counter;
+      }
+    }
+  }
+}
+
+void prime_partition::operator()(int num) {
+  if(num < minPrime || num > maxPrime)
+    return;
+  find_partition(num);
+  vals.clear();
+}
+
+bool prime_partition::find_partition(int num) {
+  set<int>::iterator upper_bound = last;
+  set<int>::iterator lower_bound = last;
+  int sum=0;
+  for(int i = 0; i < vals.size(); ++i)
+    sum += vals.at(i);
+  if(vals.size() == 2+(num%2))
+  {
+    if(sum == num) {
+      for(int i = 0; i < vals.size(); ++i)
+        cout << vals.at(i) << ' ';
+      cout << '\n';
+      return true;
+    }
+    return false;
+  }
+  
+  //Create testing iterators
+  sum = num-sum;
+  for(int i = sum; upper_bound == last; ++i)
+  {
+    upper_bound = pset.find(i);
+    upper_bound = (upper_bound == pset.end()) ? last : upper_bound;
+  }
+  for(int i = sqrt(sum); lower_bound == last; ++i)
+  {
+    lower_bound = pset.find(i);
+    lower_bound = (lower_bound == pset.end()) ? last : lower_bound;
+  }
+  
+  // Check the prime larger than the remainder
+  for(; *upper_bound >= *lower_bound; ++lower_bound)
+  {
+    cout << "Trying " << *lower_bound << " with " << sum << " as a remainder.\n";
+    vals.push_back(*lower_bound);
+    if(find_partition(num))
+      return true;
+    vals.pop_back();
+  }
+  return false;
+}
---
+++ 202/labs/7/makefile
@@ -0,0 +1,9 @@
+
+CC = g++
+CFLAGS = -Wall -g
+
+Goldbach: Goldbach.cpp 
+	$(CC) $(CFLAGS) -o $@ $<
+
+clean:
+	rm -f Goldbach
